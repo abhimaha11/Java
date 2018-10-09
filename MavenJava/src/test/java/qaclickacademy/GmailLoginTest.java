32c3f404ece5baff73a53c4884408d91a7a97619
tm_sathyav <tm_sathyav@INBASDLP04331.singtel.corp.root>
2018-10-09 15:52:49 +0530

    commit on 9th sept.

--- MavenJava/src/test/java/qaclickacademy/GmailLoginTest.java
+++ MavenJava/src/test/java/qaclickacademy/GmailLoginTest.java
@@ -1,7 +1,5 @@
 package qaclickacademy;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -9,24 +7,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.Test;
 
-public class GmailLoginTest {
+public class GmailLoginTest extends ExcelDrivenTest{
 	
 	@Test
 	public void GmailLogin()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:/Users/kudyar/chromedriver/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--incognito");
 		driver.manage().window().maximize();
-		driver.get("https://www.facebook.com/");
-		driver.findElement(By.xpath("//*[@type='email']")).sendKeys("a.mahajan11");
-		driver.findElement(By.xpath("//*[@type='password']")).sendKeys("#bhishek08");
-		driver.findElement(By.xpath("//*[@value='Log In']")).click();
-	}
-	
+		for (int i=1; i<=3; i++)
+		{
+			for (int j=1; j<=2; j++)
+			{
+				driver.get("https://www.facebook.com/");
+				driver.findElement(By.xpath("//*[@type='email']")).sendKeys("a.mahajan11");
+				driver.findElement(By.xpath("//*[@type='password']")).sendKeys("#");
+				driver.findElement(By.xpath("//*[@value='Log In']")).click();
+			}
+		}
 		
-			
-	
-
+	}
 }
--- MavenJava/src/test/java/qaclickacademy/GoogleSearchDropDownList.java
+++ MavenJava/src/test/java/qaclickacademy/GoogleSearchDropDownList.java
@@ -16,7 +16,7 @@ public class GoogleSearchDropDownList {
 	@Test
 	public void GoogleSearch()
 	{
-		System.setProperty("webdriver.chrome.driver", "C:/Users/kudyar/chromedriver/chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"//chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.google.com/");
